4963a67348f70e237251671400958f2aa723786d
Nusrat Shauoly <61565311+nusratshauoly@users.noreply.github.com>
2022-10-12 03:42:41 +0600

    module-18 er 18.4 is complete

--- module-18/variable.js
+++ module-18/variable.js
@@ -22,9 +22,54 @@
 // //new variable name diye ager variable ta boshano jay
 // var vowelsCount = vowels.length;
 // console.log(vowelsCount);
-// // array index er count 0 diye shuru hoy 0,1,2,3,4 mane koyta elemment ache
+// // array element er index/position er count 0 diye shuru hoy 0,1,2,3,4 mane koyta elemment ache
 
-//video-18.3: array index
-var bookPages = ['kite', 'runner', 'thousand', 'splendid', 'sun'];
-var kiteIndex = bookPages.indexOf(kite);
-console.log(kiteIndex);
+//video-18.3: array index (array element ta koto number e ase er position ber korar jonno)
+//index string er o hote pare
+// var books = ['kite', 'runner', 'thousand', 'splendid', 'sun'];
+// var kiteIndex = books.indexOf('kite');
+// console.log(kiteIndex);
+// //index number er o hote pare
+// var numbers = [45, 98, 65, 72, 52, 63];
+// var index = numbers.indexOf(72);
+// console.log(index);
+// jodi element jodi array te na thake tokhon output (-1) dekhabe
+// var index1 = numbers.indexOf(152);
+// console.log(index1);
+// // 0,1,2,3 erokom array er position e kon element ache shetao ber korte parbo
+// var books1 = ['kite', 'runner', 'thousand', 'splendid', 'sun'];
+// var secondIndex = books1[2];
+// console.log(secondIndex);
+// // jodi emon position dei jekhane kono man nei tahole undefined dekhabe
+// var sixthIndex = books1[6];
+// console.log(sixthIndex);
+// array er element new element diye replace o korte parbo
+// var numbers1 = [45, 98, 65, 72, 52, 63];
+// console.log(numbers1);
+// numbers1[1] = 95;
+// numbers1[4] = 21;
+// console.log(numbers1);
+
+// video-18.4: add new element is => push, remove element is => pop
+// push => add element in a array : array er shobcheye last element always add hobe (jotobar pop korbo 1 ta kore element add hobe)
+// var lastBench = ['ami', 'tumi', 'she'];
+// lastBench.push('he');
+// lastBench.push('you');
+// lastBench.push('they');
+// console.log(lastBench);
+
+// var friendsAge = [11, 13, 15, 17];
+// friendsAge.push(16);
+// console.log(friendsAge);
+
+// // pop => remove element from an array : array er shobcheye last element always delete hobe (jotobar pop korbo 1 ta kore element remove hobe)
+// friendsAge.pop();
+// friendsAge.pop();
+// console.log(friendsAge);
+// // last item jeta remove korbo oitao show korar jonno aladda variable declare korte hobe
+// var lastItem = friendsAge.pop();
+// console.log(friendsAge);
+// console.log(lastItem);
+
+
+// video- 18.5 : 
