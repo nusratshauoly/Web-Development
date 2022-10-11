65cc833e90c63b1787deaecca6757ebf8a5a7098
Nusrat Shauoly <61565311+nusratshauoly@users.noreply.github.com>
2022-10-11 15:41:46 +0600

    module-18 er 18.2 is complete

---
+++ module-18/variable.js
@@ -0,0 +1,30 @@
+// //from module-17
+// //Number
+// var watchPrice = 120;
+// watchPrice = 125;
+// //string
+// var userName = 'Sogir Uddin';
+// //boolean
+// var isExpensive = true;
+// //integer : 1 2 3 4 5  6  7 8
+// //float : 2.523 4.216 6.43
+
+// // video-18.2: array
+// var friendsAge = [11, 13, 17];
+// var muriFee = [34, 234, 3];
+// var nayikas = ['mahi', 'apu', 'mimi'];
+// var oddNumbers = [1, 3, 5, 7, 9];
+// var vowels = ['a', 'e', 'i', 'o', 'u'];
+// console.log(vowels);
+// console.log(oddNumbers);
+// // jodi jante chai array er vitor koyta element ache
+// console.log(oddNumbers.length);
+// //new variable name diye ager variable ta boshano jay
+// var vowelsCount = vowels.length;
+// console.log(vowelsCount);
+// // array index er count 0 diye shuru hoy 0,1,2,3,4 mane koyta elemment ache
+
+//video-18.3: array index
+var bookPages = ['kite', 'runner', 'thousand', 'splendid', 'sun'];
+var kiteIndex = bookPages.indexOf(kite);
+console.log(kiteIndex);
