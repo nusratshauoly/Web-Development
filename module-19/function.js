7673aa2cbdf0a3a432910588e64acc360ee6af7b
Nusrat Shauoly <61565311+nusratshauoly@users.noreply.github.com>
2022-10-16 16:03:38 +0600

    module-19 is added

---
+++ module-19/function.js
@@ -0,0 +1,58 @@
+// // video-19.1
+// // 1. variable
+//  var favouriteBook = 'kite runner';
+// // 2. array
+// var bookList = ['positiong', 'hooked', 'start with why', 'shoe dog'];
+
+// // shoe dog ook kon positiion e ase
+// var shoeDogIndex = bookList.indexOf('shoe dog');
+// // 1 no position  er book tta change kore  new  book name boshabo
+// bookList[1] = 'story Brand';
+
+// // new elemennt add
+// bookList.push('small giants');
+// bookList.pop();
+
+// //3.conditionals
+// if(bookList[1] == 'hooked'){
+//     console.log('i  am hooked');
+// }
+// else{
+//     console.log('i am not hooked');
+// }
+
+// //4.loop
+// // while loop
+// var i = 0; //loop variale
+// while(i < 15){
+//     console.log(i);
+//     console.log('looping');
+//     i++;
+// }
+
+// // for loop
+// for(var i = 0; i < 15; i++){
+//     console.log(i);
+
+// }
+
+// // video-19.2
+// // function: function e ki kaj korbo shei onujayi nam dite hobe: meaningful name . then  parenthesis use korte hobe (). then {  er vitor ki ki kaj hobe ta likhbo }
+// // function declaration er por call korte  hobe noyto kaj hobena
+// // function declaration
+// function turnOnFan(){
+//      console.log('walk towards the switch');
+//      console.log('press the switch');
+// }
+// // function call
+// turnOnFan();
+
+// console.log('eat breakfast');
+// console.log('Drink tea');
+// console.log('take a shower');
+// turnOnFan();
+// turnOnFan();
+
+// video-19.3: 
+// function parameter
+ 
