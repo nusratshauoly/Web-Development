a8062569a78328472a2b1c9c7753e31f86fd612c
Nusrat Shauoly <61565311+nusratshauoly@users.noreply.github.com>
2022-10-20 03:24:52 +0600

    module 20.5 is complete

---
+++ module-20.5/revision.js
@@ -0,0 +1,18 @@
+// core concepts
+// 1.variable(var, let , const)
+// (search on google : javascript var, let , const)
+
+
+// 2.array 
+// 3. loop (while, for),
+//4. conditionals {( 6 types : if,else,less than<,greater then >, equals to +, not equals to !=, greater then or equal >= , less than or equal <=, and && , or ||)}, 
+//5. function {(kivabe declare kore, kivabe parameter ney, kivabe function call kore , kivabe parameter pass kore, function theke kivabe return dey, )}, 
+// 6. previous problem and homework gulo and google theke aro same problem er soution ber kora
+// 7.problem : inch to feet , leap year, odd even check, factorial(while, for) diye try kora
+//8.New home work : age nije try korte hobe
+//** 8.1: celcious theke farenheit e convert korar formula and problem solving (celcious to farenheit) ,
+//** 8.2: (farenheit to celcious)
+//**8.3: number to grade convert (if,else,else if) diye (example: 90 input dile A/B/C/D grade bole dibe)
+//**8.4: simple interest calculate (koto tk dile koto tk interet and principle ashbe ta show korbe) , interest 5%/6% erokom dite hobe and first parameter principle money thakbe, second parameter hobe interest rate , third parameter hobe koto year or time,
+//formula search diye ber korte hobe
+//100 tk 5% interest e 5 years e koto tk hobe and principle koto hobe
