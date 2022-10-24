6d0fbffbf6291e9065d852be92e44a95e90ce9e8
Nusrat Shauoly <61565311+nusratshauoly@users.noreply.github.com>
2022-10-24 09:08:09 +0600

    module-22.5 is complete

---
+++ module-22.5/index.js
@@ -0,0 +1,24 @@
+// 1. comment kore kichu likha (single line , multiline comment)
+// 2. write 3 variable (1.number type variable, 2.string type variable, 3.boolean type variable)
+// 3. 2 variables declared using (let, const)
+// 4. simple math operations :  +, -, *, /, % number diye calculate korbo . number gulo variable e rekhe calculate kore console.log korbo
+// 5. comparison: 2 ta variable er moddhe compare korbo (=,<,>,!=,<=,>=)
+// 6. two conditions : 1.fulfil both conditions(&&), 2.fulfill atleast one condition (||)
+// 7.if, else, if-else, else if condition likha
+// 8.while loop: 7 to 19 : odd numbers ber korte hobe (bijor number)
+// 9. declare an array : number of elements : 4th position e element k add, remove, push, pop , index, count  and update and change kora, check specific value exits in the array
+// 10. use any for loop to display to every elements of an array
+// 11.you have an array of numbers. display only the numbers that are bigger than 80 (challenge)
+// 12. write a function that takes three numbers and return multiplication of the three numbers. (3 ta number parameter e input hishebe nibo and return korbo.)
+//13. declare an object and change any property of that object
+
+// video-22-5.2: problem solving checklist:
+// task 1: conversion : function name should be (feetToInch) : parameter e feet nibo and return feet theke koto inch hoy sheta dibo
+// task 2: conversion: (centimeter k meter e convert korte hobe).function name (centimeterToMeter)
+
+// task 3: calculation (object use korte hobe): function name (pageRequirements) . 1 jon publisher er 3 ta book
+// book1 has 100 pages, book2 has 200 pages, book3 has 300 pages. input parameter hishebe 3 ta boi er page number deya thakbe and boi koyta chapabe ta deya thakbe ist book 5ta, 2nd book 7ta, 3rd book 10 ta tahole total kotota page lagbe.
+
+// task 4 : [object and if elese condition use korbo] function name (bestFriend). input parameter hishebe array deya thakbe jekhane friends list thakbe er moddhe theke jei friend er name shobcheye boro sheta return korte hobe. string return korte hobe.
+
+// task 5: 1 ta array ache and er moddhe onekgulo number ache, but jodi array te kono negetive number pay tahole shathe shathe array ta stop hoye jabe. function name will be (onlyPositive). will stop the loop if the array has any negative number and returns all the positive number before the negetive number.
