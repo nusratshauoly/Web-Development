7c9d07fff52a9a8226559d1e863af08ec48210a4
Nusrat Shauoly <61565311+nusratshauoly@users.noreply.github.com>
2023-02-02 14:53:48 +0600

    module-18

---
+++ milestone-4-practice/module-18:loop-and-practice-problems/index.js
@@ -0,0 +1,8 @@
+var roastGiven = 0;
+while(roastGiven < 7){
+    console.log('roast den, please');
+    // each time 1 kore barbe;
+    //roastGiven = roastGiven + 1;
+    console.log(roastGiven);
+    roastGiven++;
+}
