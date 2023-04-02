f606ddb67005ed09075aa603c3c5d02907923fc4
Nusrat Shauoly <61565311+nusratshauoly@users.noreply.github.com>
2023-04-03 01:51:59 +0600

    module 17.4 complete

---
+++ milestone-4-practice/module-17-introduction-to-javascript/practice.js
@@ -0,0 +1,15 @@
+console.log(3);
+// video- 17.3
+var price = 30;
+var age = 25;
+var temperature = 38; 
+
+// video - 17.4
+// numeric
+var person = 'nusrat';
+var name = "nusrat jahan";
+var serious = true;
+var isPassed = false;
+
+
+// video- 17.5
