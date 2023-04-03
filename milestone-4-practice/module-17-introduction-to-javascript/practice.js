4acf847eddec85589c5e48fa26d538a5cd015a4f
Nusrat Shauoly <61565311+nusratshauoly@users.noreply.github.com>
2023-04-03 12:18:20 +0600

    module - 17

--- milestone-4-practice/module-17-introduction-to-javascript/practice.js
+++ milestone-4-practice/module-17-introduction-to-javascript/practice.js
@@ -12,4 +12,87 @@ var serious = true;
 var isPassed = false;
 
 
-// video- 17.5
+// video- 17.6
+var onionPrice = 20;
+var eggPrice = 10;
+var totalPrice = onionPrice + eggPrice;
+var priceDifference = onionPrice - eggPrice;
+console.log(onionPrice);
+console.log(eggPrice);
+console.log(totalPrice);
+console.log(priceDifference);
+// addition
+console.log(onionPrice + eggPrice)
+// subtraction
+console.log(onionPrice - eggPrice);
+// multiplication
+var orangePrice = 20;
+var orangeQuantity = 7;
+var totalPrice = (orangePrice * orangeQuantity);
+console.log(totalPrice);
+
+// divison
+var money = 500;
+var player = 10;
+var eachPlayer = money / player;
+console.log(eachPlayer);
+
+
+// video-17.7
+
+var tomatoPrice = 25;
+tomatoPrice = tomatoPrice + 30;
+tomatoPrice += 30;
+tomatoPrice -= 30;
+tomatoPrice *= 30;
+console.log(tomatoPrice);
+
+var age = 15;
+age = age + 1;
+age++;
+console.log(age);
+
+// video - 17.8
+var firstName = 'jennie';
+var lastName = 'kim';
+var fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+
+var price1 = '20';
+var price2 = '22';
+var totalPrice = price1 + price2;
+console.log(totalPrice);
+var price1Number = parseInt(price1);
+console.log(price1Number);
+var gpa = 3.4;
+var gpaNumber = parseFloat(gpa);
+console.log(gpaNumber);
+
+// video - 17.9:
+
+var price = 100;
+console.log(typeof price);
+
+var price1 = '100';
+console.log(typeof price1);
+
+var isRaining = true;
+console.log(typeof isRaining);
+
+var isRomantic;
+console.log(typeof isRomantic);
+
+var firstNumber = 0.1;
+var secondNumber = 0.2;
+var sum = firstNumber + secondNumber;
+console.log(sum.toFixed(3));
+console.log(parseFloat(sum.toFixed(3)));
+
+
+var mangoes = 7;
+var person = 2;
+var dividing = mangoes / person;
+console.log(dividing);
+var remaining = mangoes % person;
+console.log(remaining);
