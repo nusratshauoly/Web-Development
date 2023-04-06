2848df053a98288ec6237e6980fe37b15ab44348
Nusrat Shauoly <61565311+nusratshauoly@users.noreply.github.com>
2023-04-06 13:05:46 +0600

    module- 19: loop and practice problem

--- milestone-4-practice/module-19:loop-and-practice-problems/index.js
+++ milestone-4-practice/module-19:loop-and-practice-problems/index.js
@@ -1,4 +1,5 @@
 //19.3
+
 // while condition
 //1.loop variable
 //2. condition inside while
@@ -33,6 +34,7 @@ while(number <= 10){
 }
 
 // 19.4: 
+
 // 1 to 100 all odd number
 var number = 1;
 while(number <= 100){
@@ -83,3 +85,82 @@ for(var i = 0; i < items.length; i++){
     var item = items[i];
     console.log(item);
 }
+
+// 19.6: break and continue
+// break condition using for loop
+for(var i = 0; i <= 20; i++){
+    console.log(i);
+    if(i > 10){  //kono condition e break likhbo (condition er beshi hole break bondho hoye jabe) .jodi(i > 100) dei tahole 20 porjonto for loop er man porjonto jeye condition break hoye jabe.
+      break;
+    }
+}
+
+// break condition using while loop
+var roastGiven = 0;
+while(roastGiven < 10){
+    console.log('roast please!');
+    roastGiven++;
+    if(roastGiven > 4){
+       break;
+    }
+}
+
+// example1: pen e debag korar shomoy break korbe means pen show korbena.
+var items = ["bottle", "mouse", "pen"];
+for(var i = 0; i < items.length; i++){
+    var item = items[i];
+    if(item == 'pen'){
+         break;
+    }
+    console.log(item);
+}
+
+// example2: break statement
+var numbers = [45, 87, 89, 56, 32, 170, 51, 25, 51, 32];
+
+for(var i = 0; i < numbers.length; i++){ //array te jokhon 170 ashbe tokhon e break korbe. 
+    var number = numbers[i];
+    if(number > 100){
+      break;
+    }
+    console.log(number);
+}
+
+
+// example 3 : continue statement/50 er cheye boro output gulo skip korbe ba loop continue korbe output e dekhabe na
+var numbers = [45, 87, 89, 56, 32, 170, 51, 25, 51, 32];
+
+for(var i = 0; i < numbers.length; i++){ //array te jokhon  ashbe tokhon e continue korbe. 
+    var number = numbers[i];
+    if(number > 50){
+      continue;
+    }
+    console.log(number);
+}
+
+
+// 19.7 for and while loop in a reverse way
+
+// while inverse way
+var num = 1;
+while(num <= 10){
+    console.log(num);
+    num++;
+}
+// while reverse way
+var num = 10;
+while(num >= 1){
+    console.log(num);
+    num--;
+}
+
+// for inverse way
+for(var i = 0; i <= 10; i++){
+    console.log(i);
+}
+// for reverse way
+for(var i = 10; i >= 1; i--){
+    console.log(i);
+}
+
+// video - 19.8: 
