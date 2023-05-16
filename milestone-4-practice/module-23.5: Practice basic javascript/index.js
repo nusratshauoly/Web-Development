69d6250751ae9d30e6d701c3772eda1889bde7be
Nusrat Shauoly <61565311+nusratshauoly@users.noreply.github.com>
2023-05-16 20:35:19 +0600

    module - 23.5: practice basic javascript

---
+++ milestone-4-practice/module-23.5: Practice basic javascript/index.js
@@ -0,0 +1,23 @@
+// Module- 23-5-2: Simple Problem-solving checklist and practice:
+
+/*
+1. feet to inch 
+function name (feetToInch) dibo
+
+2. centimeter to meter
+function name (centimeterToMeter) dibo
+
+3. count paper
+// book1 ---> 100
+// book2 ---> 200
+// book3 ---> 300
+function name (paperRequirements) dibo
+
+4. ['sajid', 'mamun', 'kamal', 'jubayer bin rashed', 'Chinku']
+function name (bestFriend) dibo
+(jei bane ta shobchye boro sheta return korbo)
+
+5. onlyPositive
+   [45, 87, 96, 11, 63, -56, 71, 28];
+*/
+
