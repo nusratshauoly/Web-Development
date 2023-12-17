60499b8103a2ae9adc3d4f2aadf7dcfb2799795e
Nusrat Shauoly <61565311+nusratshauoly@users.noreply.github.com>
2023-12-17 09:46:29 +0600

    modal window js dom practice

--- JS-Course_udemy/Sec-7-06-Modal/starter/practice.js
+++ JS-Course_udemy/Sec-7-06-Modal/starter/practice.js
@@ -1,8 +1,9 @@
 'use strict';
-const modal = document.querySelector('.modal');
+
 const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.close-modal');
-const btnsOpenModal = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const btnsOpenModal = document.querySelectorAll('.open-modal');
+const btnsCloseModal = document.querySelector('.close-modal');
 
 const openModal = function () {
   modal.classList.remove('hidden');
@@ -16,13 +17,11 @@ const closeModal = function () {
 
 for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener('click', openModal);
-
-btnCloseModal.addEventListener('click', closeModal);
+btnsCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
+// escape key
 document.addEventListener('keydown', function (e) {
-  // console.log(e.key);
-
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
