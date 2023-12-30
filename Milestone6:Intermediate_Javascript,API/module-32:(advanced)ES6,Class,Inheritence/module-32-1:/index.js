b2870434e9348c14c9143b0bdf299f9630a07e4f
Nusrat Shauoly <61565311+nusratshauoly@users.noreply.github.com>
2023-12-30 21:34:41 +0600

    module-32: nested object, optional chaining

---
+++ Milestone6:Intermediate_Javascript,API/module-32:(advanced)ES6,Class,Inheritence/module-32-1:/index.js
@@ -0,0 +1,54 @@
+// 1. var let const (use let and const)
+const numbers = [12, 546, 56, 90]; // jokhon variable declare korbo tokhon e value dite hobe and reassign korte parbo na.
+
+
+
+
+// 2. let a number reassign korte parbo
+let salary = 450;
+salary = 455;
+
+
+// 3. default parameters
+function calculateSalary(salary, tax = 0.25, bonus = 0) {
+   const remaining = salary - salary * tax;
+   const total = remaining + bonus;
+   return total;
+}
+
+
+// 4. template string
+const element = `
+   <div>
+       <h3>Name: </h3>
+       <p>Address: </p>
+       <p>Salary: ${calculateSalary(10000, 0, 0)}</p>
+       <p>Others: ${numbers[2]}</p>
+   </div>
+`
+
+
+
+
+// 5. Arrow function
+const doubleIt = x => x * 2;
+const calculateSalary2 = (salary, tax, bonus) => salary - salary * tax + bonus;
+
+
+// 6. spread operator
+const ages = [11, 13, 15, 14, 10, 16];
+const newAges = [...ages, 22, 24, 23];
+console.log(newAges);
+
+
+
+
+// 7. destructuring (...c is rest operator => x, y, z er por baki shobgulo (c) te add hobe )
+
+
+// object er moddhe position name and variable name same hote hobe
+const {x, y, z ...c} = {x: 45, y: 12, z: 33, name: 'Sakib Al Hasan', salary: 450000}
+
+
+// Array er moddhe position and variable name same na holeo hobe
+const [a, b, ...r] = [12, 45, 21, 65, 98];
---
+++ Milestone6:Intermediate_Javascript,API/module-32:(advanced)ES6,Class,Inheritence/module-32-2/index.js
@@ -0,0 +1,47 @@
+// 1. kono object jokhon khujbo array er moddhe theke
+// ekhane array er moddhe ekta element and shei element er moddhe (id / name / job) egulo holo ekekta object.
+const users = [
+    {id: 1, name: 'abul', job: 'doctor'}
+]
+
+// ekhane array er vitor 1 ta element and er position number is 0. [0] position er object value gulo ber korte chai...
+console.log(users[0].name);
+
+
+// 2. another array
+const data = {
+    count: 5000,
+    data: [
+        {id: 1, name: 'babul', job: 'leader'},
+        {id: 2, name: 'dabul', job: 'leader'},
+    ]
+}
+console.log(data);
+console.log(data.count);
+// data object er vitor data array er first[0] position element er job ber korte hobe and egulo k 1 ta variable er moddhe rakhbo
+const firstJob = data.data[0].job
+console.log(firstJob);
+
+
+// 3. another Object
+const user = {
+    id: 5001,
+    name: 'Thomas alva edison',
+    address: {
+        street: {
+            first: '35/A kochukhet lane',
+            second: 'third floor',
+            third: 'right side'
+        },
+        postOffice: 'cantonment',
+        city: 'Dhaka'
+    }
+}
+
+// user kon floor e thake
+const userFloor = user.address.street.second;
+console.log(userFloor);
+
+// (using (? => optional chaining)) : to see porer element e kono value ache kina
+const userFloor = user.address.street?.second;
+console.log(userFloor);
