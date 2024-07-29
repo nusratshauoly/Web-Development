41f55cf7804386bd1e3449ef062f6b12f84d4604
Nusrat Shauoly <61565311+nusratshauoly@users.noreply.github.com>
2024-07-29 15:51:44 +0600

    geometry resources practice

---
+++ lws-DOM-JS/vid-2:Analyze-the-DOM.js
@@ -0,0 +1,20 @@
+// vid - 2: Analyze the DOM - 
+console.log(document.title);
+console.log(document.dir);
+console.log(document.URL);
+console.log(document.domain);
+console.log(document);
+console.log(document.body);
+console.log(document.head);
+
+console.log(document.title = 'DOM - Play with DOM');
+
+console.log(typeof (document.all)); // (document.all) is not an array it is an object whose type is (HTMLAllCollection)
+for (let element of document.all) {
+    console.log(element);
+}
+
+console.log(document.all[5]);
+
+console.log(document.forms);
+console.log(document.links);
