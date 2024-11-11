97131b4a3eb393062b5ce46c843d7e32601b5366
Nusrat Shauoly <61565311+nusratshauoly@users.noreply.github.com>
2024-11-11 18:01:13 +0600

    class 12 complete

---
+++ 100-Dev/software-enginnering-bootcamp-cohort-2/100Devs/class-13/index.js
@@ -0,0 +1,23 @@
+// condition
+const pizza = "Dominos";
+
+if (pizza === 'Papa Johns') {
+    console.log('Scram!!');
+}
+else if (pizza === "Dominos") {
+    console.log("All abroad the train to flavor town");
+}
+else {
+    console.log("What r u even doing with your life!");
+}
+
+
+// double condition
+if (name === "Leon" && status === "Ballin") {
+    // wink at camera
+}
+
+// 
+if (say === 'Saturday' || day === 'Sunday') {
+    // ti is the weekend
+}
